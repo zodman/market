d33b009f51b51bc2b4aaec8833343c8d7e4e7429
Andres Vargas <andrevargas@coca-cola.com>
2020-08-14 22:15:29 -0500

    adding more Models

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric import task
+from patchwork.transfers import rsync
+from invoke import run
+
+exclude_dirs = [".git", "node_modules", ".cache", ".github", "db.sqlite3"]
+
+
+@task
+def reinit(ctx):
+    run("rm db.sqlite3")
+    run("python manage.py migrate")
+    run("python populate.py")
+
+
+@task
+def test(c):
+    run(" coverage run manage.py test --failfast")
+    # run("coverage report -m ")
+
+
+@task
+def log(ctx):
+    run("pip-compile requirements.in -o requirements.txt")
+
---
+++ market/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+
+class MixBase:
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Food(MixBase,models.Model):
+    FOOD_TYPES = (
+        ('meat', 'Meat'),
+        ('fruit', 'Fruit'),
+        ('vegetable', 'Vegatable'),
+        ('desert', 'Desert'),
+        ('dairy', 'Dairy'),
+        ('beverage', 'Beverage'),
+        ('bread', 'Bread'),
+    )
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=11, choices=FOOD_TYPES)
+
+    def __str__(self):
+        return self.name
+
+class CartRow(MixBase, models.Model):
+    food = models.ForeignKey(Food, on_delete=models.SET_NULL)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return "CartRow {}".format(self.id)
+
+class Cart(MixBase, models.Model):
+    rows = models.ManyToManyField(CartRow, through=CartRow)
+
+    def __str__(self):
+        return "Cart {}".format(self.id)
+
+class Order(MixBase, models.Model):
+    STATUS = (
+        ('n', 'New'),
+        ('s', 'Send'),
+        ('r', 'Received'),
+        ('c', 'Cancelled'),
+    )
+    cart = models.OneToOneField(Cart)
+    # owner
+    status = models.CharField(max_length=1, choices=STATUS)
+
+
